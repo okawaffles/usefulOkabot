f5c4eceecd68a4d18f083258b7237cef80d4e76d
Eli Clark <herbin12@herbin12.com>
2022-02-11 17:31:07 -0600

    add auto delete and more tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('config.json');
+const config = require('./config.json');
 
 client.once("ready", () => {
     console.log("Online and Ready");
@@ -9,6 +9,10 @@ client.once("ready", () => {
 client.login(config.token);
 
 client.on("message", message => {
+    if(message.content.includes("<@!941422459201138718>")) {
+        message.channel.send("");
+    }
+
     if(message.content.includes("@everyone") && !message.author.hasPermission("MENTION_EVERYONE")) {
         message.delete();
         message.channel.send(`<@!${message.author.id}>, you can't ping everyone!`);
@@ -16,8 +20,15 @@ client.on("message", message => {
 
     if((message.content.includes("can you test play") || message.content.includes(".zip")) && message.content.includes("://")) { //test play scams
         message.delete();
-        message.channel.send(`<@!${message.author.id}>, your message was flagged as spam and deleted.`);
+        message.channel.send(`<@!${message.author.id}>, your message was flagged as spam and deleted.`).then(message => {
+            message.delete({timeout:5000});
+        });
     }
 
-    //if(message.content.i)
+    if(message.content.includes("nitro") && message.content.includes("free") && message.content.includes("://")) {
+        message.delete();
+        message.channel.send(`<@!${message.author.id}>, your message was flagged as spam and deleted.`).then(message => {
+            message.delete({timeout:5000});
+        });
+    }
 })
