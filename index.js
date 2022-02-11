5044697f772a45631c1e5b24a9e11efcfb894933
Eli Clark <herbin12@herbin12.com>
2022-02-11 17:59:12 -0600

    add some more functions n stuff

--- index.js
+++ index.js
@@ -8,27 +8,41 @@ client.once("ready", () => {
 
 client.login(config.token);
 
+function sendReport(type, message) {
+    let reportChannel = message.guild.channels.cache.find(channel => channel.name === "okabot-reports");
+    if(reportChannel != null) {
+        if(type === "spam") {
+            reportChannel.send(`${message.author.username}(ID:${message.author.id}) sent a message that triggered the spam filter!\n \nMessage contents:\`${message.content}\``);
+        }
+        if(type === "everyone") {
+            reportChannel.send(`${message.author.name} sent an everyone ping without permissions.\n \nMessage contents:\`${message.content}\``);
+        }
+    }
+}
+
 client.on("message", message => {
+    let msg = message.content.toLowerCase();
     if(message.content.includes("<@!941422459201138718>")) {
         message.channel.send("");
     }
 
-    if(message.content.includes("@everyone") && !message.author.hasPermission("MENTION_EVERYONE")) {
+    if((msg.includes("can you test play") || msg.includes(".zip")) && msg.includes("://") && message.author != 941422459201138718) { //test play scams
+        message.delete();
+        sendReport("spam", message);
+        message.channel.send(`<@!${message.author.id}>, your message was flagged as spam and deleted.`).then(message => {
+            message.delete({timeout:5000});
+        });
+    }
+
+    if(msg.includes("nitro") && msg.includes("free") && (msg.includes("://") || msg.includes(".com") || msg.includes(".net")) && message.author != 941422459201138718) {
+        message.delete();
+        message.channel.send(`<@!${message.author.id}>, your message was flagged as spam and deleted.`).then(message => {
+            message.delete({timeout:5000});
+        });
+    }
+
+    if(msg.includes("@everyone") && !message.author.hasPermission("MENTION_EVERYONE") && message.author != 941422459201138718) {
         message.delete();
         message.channel.send(`<@!${message.author.id}>, you can't ping everyone!`);
-    }
-
-    if((message.content.includes("can you test play") || message.content.includes(".zip")) && message.content.includes("://")) { //test play scams
-        message.delete();
-        message.channel.send(`<@!${message.author.id}>, your message was flagged as spam and deleted.`).then(message => {
-            message.delete({timeout:5000});
-        });
-    }
-
-    if(message.content.includes("nitro") && message.content.includes("free") && message.content.includes("://")) {
-        message.delete();
-        message.channel.send(`<@!${message.author.id}>, your message was flagged as spam and deleted.`).then(message => {
-            message.delete({timeout:5000});
-        });
     }
 })
